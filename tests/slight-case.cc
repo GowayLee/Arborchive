b0d983a643fe21ec1e4b62663e37cc76820ba17f
Li Haoyuan <1513624626@qq.com>
2025-02-20 21:23:54 +0800

    test: update slight-case test

--- tests/slight-case.cc
+++ tests/slight-case.cc
@@ -1,10 +1,16 @@
-int add(int a, int b) {
-    return a + b;
-}
+class foo {
+public:
+  foo() {}
+  void bar() {}
+
+  void baz() {}
+};
+
+int add(int a, int b) { return a + b; }
 
 int main() {
-    int x = 10;
-    int y = 20;
-    int z = add(x, y);
-    return z;
+  int x = 10;
+  int y = 20;
+  int z = add(x, y);
+  return z;
 }
