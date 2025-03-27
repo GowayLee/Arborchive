d19939b5e6e3fe417bceafba96ebf6c3f02e93b8
Li Haoyuan <1513624626@qq.com>
2025-03-27 17:50:09 +0800

    refactor:: remove class_sql_model.cc

--- include/model/sql/class_model.h
+++ include/model/sql/class_model.h
@@ -2,15 +2,25 @@
 #define _CLASS_MODEL_H_
 
 #include "model/sql/sql_model.h"
+#include <map>
+#include <string>
 
 class ClassSQLModel : public SQLModel {
 private:
   std::map<std::string, std::string> fields_;
 
 public:
-  ClassSQLModel(const std::string &name, const std::string &namespace_);
-  std::string getTableName() const override;
-  std::string serialize() const override;
+  ClassSQLModel(const std::string &name, const std::string &namespace_) {
+    fields_["name"] = name;
+    fields_["namespace"] = namespace_;
+  }
+
+  std::string getTableName() const override { return "classes"; }
+
+  std::string serialize() const override {
+    return "INSERT INTO classes (name, namespace) VALUES ('" +
+           fields_.at("name") + "', '" + fields_.at("namespace") + "')";
+  }
 };
 
 #endif // _CLASS_MODEL_H_
